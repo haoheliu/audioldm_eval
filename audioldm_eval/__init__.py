a041040ffcbc183f7f4f061d788ad914df7fe510
haoheliu <haoheliu@bytedance.com>
2023-06-25 05:39:19 +0800

    change the panns backbone to passt in this branch

--- audioldm_eval/__init__.py
+++ audioldm_eval/__init__.py
@@ -3,3 +3,5 @@ from .metrics.isc import calculate_isc
 from .metrics.kid import calculate_kid
 from .metrics.kl import calculate_kl
 from .eval import EvaluationHelper
+
+print("2023 -06 -22")
--- test.py
+++ test.py
@@ -3,14 +3,45 @@ from audioldm_eval import EvaluationHelper
 
 device = torch.device(f"cuda:{0}")
 
-generation_result_path = "example/paired"
-# generation_result_path = "example/unpaired"
-target_audio_path = "example/reference"
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k"
+generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k_16k"
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k_resp_librosa_16k"
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k_resp_ta_16k"
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k_resp_sox_simple__16k"
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_32k_resp_sox_16k"
 
-evaluator = EvaluationHelper(16000, device)
+target_audio_path = "/mnt/bn/lqhaoheliu/project/audioldm_eval/audio/target/audiocaps"
+# target_audio_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/testset_data/audiocaps_16k"
+# target_audio_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/testset_data/audiocaps_32k_16k_sox"
+
+evaluator = EvaluationHelper(32000, device)
 
 # Perform evaluation, result will be print out and saved as json
 metrics = evaluator.main(
     generation_result_path,
     target_audio_path,
 )
+
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/latent_diffusion/22_is_audiomae_helpful_or_not/2023_06_20_clap_1000_steps_32k/val_247520_16k"
+# # generation_result_path = "example/unpaired"
+# target_audio_path = "/mnt/bn/lqhaoheliu/project/audioldm_eval/audio/target/audiocaps_16k"
+
+# evaluator = EvaluationHelper(16000, device)
+
+# # Perform evaluation, result will be print out and saved as json
+# metrics = evaluator.main(
+#     generation_result_path,
+#     target_audio_path,
+# )
+
+# generation_result_path = "/mnt/bn/lqhaoheliu/project/audioldm_eval/audio/output/2023_05_20_audiomae_crossattn_audiocaps_val_185640"
+# # generation_result_path = "example/unpaired"
+# target_audio_path = "/mnt/bn/lqhaoheliu/project/audio_generation_diffusion/log/testset_data/audiocaps_16k"
+
+# evaluator = EvaluationHelper(16000, device)
+
+# # Perform evaluation, result will be print out and saved as json
+# metrics = evaluator.main(
+#     generation_result_path,
+#     target_audio_path,
+# )
